ef6dfbf546d05ad616a7331233cabca0804489ce
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-09 11:53:03 +0200

    [GFE]: add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://emmanuelplougoulm.github.io/components_library/',
-  base: '/components_library/'
+  base: '/components_library/',
+  integrations: [react()]
 });
