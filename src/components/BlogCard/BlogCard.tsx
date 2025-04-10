9251eaf6059fca92854fc770b2e81be3a21d6111
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-10 14:30:12 +0200

    [GFE]: add BlogCard perf optimization

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './BlogCard.css';
 import Badge from '../Badge/Badge';
 import ArrowRight from '../../assets/icons/arrow-right.svg';
 
 type BlogCardProps = {
   badgeStatus: 'neutral' | 'success' | 'error' | 'warning' | 'brand';
+  badgeLabel: string;
   image: string;
   title: string;
   excerpt: string;
@@ -13,22 +14,25 @@ type BlogCardProps = {
 };
 
 const BlogCard: React.FC<BlogCardProps> = ({
-  badgeStatus = 'neutral',
-  image = '',
-  title = 'title',
-  excerpt = 'excerpt',
+  badgeStatus,
+  badgeLabel,
+  image,
+  title,
+  excerpt,
   btnLabel = 'button',
   btnFn
 }) => {
   return (
     <div className="blog_card" role="article" aria-labelledby="blog-card-title">
-      <img
-        src={image}
-        className="blog_card_image"
-        alt={title || 'Blog image'}
-      />
+      {image && (
+        <img
+          src={image}
+          className="blog_card_image"
+          alt={title ? `Image for ${title}` : 'Blog image'}
+        />
+      )}
       <div className="blog_card_content">
-        <Badge color={badgeStatus} label="Interior" size="md" />
+        <Badge color={badgeStatus} label={badgeLabel} size="md" />
         <h2 id="blog_card_title" className="blog_card_title ">
           {title}
         </h2>
@@ -38,7 +42,7 @@ const BlogCard: React.FC<BlogCardProps> = ({
         <button
           aria-label={`Perform action: ${btnLabel}`}
           className="blog_card_button"
-          onClick={() => btnFn()}
+          onClick={() => btnFn?.()}
         >
           {btnLabel}
           <img src={ArrowRight.src} alt="Arrow Right Icon" aria-hidden="true" />
@@ -48,4 +52,4 @@ const BlogCard: React.FC<BlogCardProps> = ({
   );
 };
 
-export default BlogCard;
+export default memo(BlogCard);
