26c0a38c5060f79e6e4c929076d3a5cd57577cd4
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-10 14:54:59 +0200

    [GFE]: enable svg import as ReactComponent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from 'astro/config';
-
+import svgr from 'vite-plugin-svgr';
 import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://emmanuelplougoulm.github.io/components_library/',
   base: '/components_library/',
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [
+      svgr({
+        svgrOptions: {
+          plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+          svgoConfig: {
+            plugins: [
+              'preset-default',
+              { name: 'removeViewBox', active: false },
+              'removeTitle',
+              'removeDesc'
+            ]
+          }
+        }
+      })
+    ]
+  }
 });
