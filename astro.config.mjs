d6c2c0be86a2203ec9661ea0cb7f66c545aa2f9b
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-12 20:22:36 +0200

    [GFE]: add svg icons + setup in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import svgr from 'vite-plugin-svgr';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://emmanuelplougoulm.github.io/components_library/',
   base: '/components_library/',
-  integrations: [react(), vue()],
+  integrations: [react(), vue(), icon()],
   vite: {
     plugins: [
       svgr({
