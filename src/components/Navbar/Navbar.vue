b3fc68951a82b8ce261d2529532f08f9eb7fd091
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-13 14:26:01 +0200

    [GFE]: add page + component skeletons

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,21 @@
+<template></template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps<{
+  // label?: string;
+  // size: 'md' | 'lg' | 'xl' | 'xxl';
+  // variant:
+  //   | 'primary'
+  //   | 'secondary'
+  //   | 'tertiary'
+  //   | 'link-color'
+  //   | 'link-gray'
+  //   | 'destructive';
+  // disabled?: boolean;
+  // onClick?: () => void;
+}>();
+</script>
+
+<style></style>
