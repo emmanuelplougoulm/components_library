e9707dd4049082f3a8683afcd4f7952a31949705
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-13 14:59:20 +0200

    [GFE]: add media queries for Navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <header class="navbar-container"></header>
+</template>
 
 <script lang="ts" setup>
 import { defineProps } from 'vue';
@@ -18,4 +20,27 @@ defineProps<{
 }>();
 </script>
 
-<style></style>
+<style>
+header {
+  /* all: unset; */
+}
+
+.navbar-container {
+  height: 84px;
+  background-color: aqua;
+}
+
+/* TABLET */
+@media screen and (max-width: 768px) {
+  .navbar-container {
+    background-color: blue;
+  }
+}
+
+/* MOBILE */
+@media screen and (max-width: 375px) {
+  .navbar-container {
+    background-color: violet;
+  }
+}
+</style>
