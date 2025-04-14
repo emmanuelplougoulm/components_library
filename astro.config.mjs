843e928741e51b452dc6a5afde4f71f0a321319d
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-14 17:50:27 +0200

    [GFE]: update astro config for svg in vue + vue devtools enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import svgr from 'vite-plugin-svgr';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import icon from 'astro-icon';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
   site: 'https://emmanuelplougoulm.github.io/components_library/',
   base: '/components_library/',
-  integrations: [react(), vue(), icon()],
+  integrations: [react(), vue(), icon(), svgLoader()],
   vite: {
     plugins: [
       svgr({
+        define: {
+          __VUE_PROD_DEVTOOLS__: JSON.stringify(true)
+        },
         svgrOptions: {
           plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
           svgoConfig: {
