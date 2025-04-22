162f6208c9bac60ed3bda57cf26760cb4d80ae62
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-22 15:20:49 +0200

    [GFE]: <Tabs/> add states styles to single tab

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="tabs">
-    <ul class="tabs__header">
-      <li v-for="(tab, index) in tabs" :key="tab.title">
-        {{ tab.title }}
+    <ul class="tabs__header" role="tablist">
+      <li v-for="(tab, index) in tabs" :key="tab.title" role="tab-item">
+        <button :class="[`tab`, { active: index === activeIndex }]">
+          {{ tab.title }}
+        </button>
       </li>
     </ul>
     <div class="tabs__content">
@@ -24,25 +26,36 @@ defineProps<TTabsProps>();
 </script>
 
 <style>
+:root {
+  --color-border: #e5e5e5;
+  --color-background: #ffffff;
+  --color-background-hover: #fafafa;
+
+  --color-text: #525252;
+  --color-text-active: #171717;
+}
+
 * {
   box-sizing: border-box;
+}
+
+button {
+  all: unset;
 }
 
 ul,
 li {
   margin: 0;
   padding: 0;
+  list-style: none;
 }
 
 /* BASE STYLES*/
 .tabs__header {
   display: flex;
   justify-content: flex-start;
-
   gap: 8px;
-  /* padding: 10px 16px; */
   border-radius: 4px;
-  border: 1px blue solid;
 }
 
 .tabs__header .tab {
@@ -54,12 +67,31 @@ li {
   border-radius: 4px;
   cursor: pointer;
   text-transform: capitalize;
+  background-color: var(--color-background);
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: var(--color-text);
 }
 /* ACTIVE */
+.tabs__header .tab.active {
+  border: 0.5px solid var(--color-border);
+  box-shadow:
+    0px 1px 2px -1px rgb(0 0 0 / 0.1),
+    0px 1px 3px 0 rgb(0 0 0 / 0.1);
+  color: var(--color-text-active);
+}
 
 /* HOVER */
-
+.tabs__header .tab:hover {
+  background-color: var(--color-background-hover);
+  color: var(--color-text);
+}
 /* FOCUS */
-
-/* DISABLED*/
+.tabs__header .tab:focus {
+  background-color: var(--color-background-hover);
+  color: var(--color-text);
+  box-shadow: 0px 0px 0px 4px
+    rgb(0.2666666805744171 0.2980392277240753 0.9058823585510254 / 0.12);
+}
 </style>
