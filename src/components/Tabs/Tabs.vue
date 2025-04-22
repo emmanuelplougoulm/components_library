b94eee2fe2d0ad59763997519f7194e660f610aa
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-22 13:55:00 +0200

    [GFE]: setup Astro page for Tabs + add skeleton for component

---
+++ src/components/Tabs/Tabs.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import BaseIcon from '@components/BaseIcon/BaseIcon.vue';
+
+type TTabsProps = {};
+
+defineProps<TTabsProps>();
+</script>
+
+<style></style>
