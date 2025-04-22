9d3351e57fd2df7f3ade999589a124d8cb962a44
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-22 16:05:53 +0200

    [GFE]: <Tabs/> CSS cleanup + px to rem

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -10,8 +10,12 @@
         </button>
       </li>
     </ul>
-    <div class="tabs__content">
-      <div v-show="index === activeIndex" v-for="(tab, index) in tabs">
+    <div class="tabs__content" role="tab-content-container">
+      <div
+        v-show="index === activeIndex"
+        v-for="(tab, index) in tabs"
+        role="active-tab-content"
+      >
         {{ tab.content }}
       </div>
     </div>
@@ -39,9 +43,9 @@ defineProps<TTabsProps>();
   --color-border: #e5e5e5;
   --color-background: #ffffff;
   --color-background-hover: #fafafa;
-
   --color-text: #525252;
   --color-text-active: #171717;
+  --color-text-content: #000000;
 }
 
 * {
@@ -60,34 +64,47 @@ li {
 }
 
 /* BASE STYLES*/
+.tabs {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
 .tabs__header {
   display: flex;
   justify-content: flex-start;
-  gap: 8px;
-  border-radius: 4px;
+  gap: 0.5rem;
+  border-radius: 0.25rem;
+}
+
+.tabs__content {
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: #000000;
 }
 
 .tabs__header .tab {
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 6px;
-  padding: 10px 16px;
-  border-radius: 4px;
+  gap: 0.375rem;
+  padding: 0.625rem 1rem;
+  border-radius: 0.25rem;
   cursor: pointer;
   text-transform: capitalize;
   background-color: var(--color-background);
   font-weight: 500;
-  font-size: 16px;
-  line-height: 24px;
+  font-size: 1rem;
+  line-height: 1.5rem;
   color: var(--color-text);
 }
+
 /* ACTIVE */
 .tabs__header .tab.active {
-  border: 0.5px solid var(--color-border);
+  border: 0.0313rem solid var(--color-border);
   box-shadow:
-    0px 1px 2px -1px rgb(0 0 0 / 0.1),
-    0px 1px 3px 0 rgb(0 0 0 / 0.1);
+    0rem 0.0625rem 0.125rem -0.0625rem rgb(0 0 0 / 0.1),
+    0rem 0.0625rem 0.1875rem 0 rgb(0 0 0 / 0.1);
   color: var(--color-text-active);
 }
 
@@ -96,11 +113,12 @@ li {
   background-color: var(--color-background-hover);
   color: var(--color-text);
 }
+
 /* FOCUS */
 .tabs__header .tab:focus {
   background-color: var(--color-background-hover);
   color: var(--color-text);
-  box-shadow: 0px 0px 0px 4px
+  box-shadow: 0rem 0rem 0rem 0.25rem
     rgb(0.2666666805744171 0.2980392277240753 0.9058823585510254 / 0.12);
 }
 </style>
--- src/components/Tabs/config.ts
+++ src/components/Tabs/config.ts
@@ -4,8 +4,16 @@ const tabs = [
     content:
       'The Account Management section provides a comprehensive view of your personal information and settings. Here, you can update your profile details, manage contact information, and customize your preferences to enhance your user experience.'
   },
-  { title: 'security', content: 'tata' },
-  { title: 'plan', content: 'titi' }
+  {
+    title: 'security',
+    content:
+      'The Security Settings section is dedicated to protecting your account and personal data. Here, you can manage various security features to ensure your information remains safe and secure.'
+  },
+  {
+    title: 'plan',
+    content:
+      'The Subscription Plan section provides details about your current plan and available upgrades. Here, you can review your plan’s benefits, manage billing information, and explore other subscription options to find the best fit for your needs.'
+  }
 ];
 
 export { tabs };
