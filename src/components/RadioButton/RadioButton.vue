eea729b604198f8a65faaffe6e95a924c401ef0a
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-25 15:24:36 +0200

    [GFE]: add <RadioButton/> with no styling

---
+++ src/components/RadioButton/RadioButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="radio" :id="inputId" name="monster" value="K" />
+  <label :for="inputId">Radio button</label>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { type TSize } from '@custom-types/types';
+
+const inputId = `input-${Math.random().toString(36)}`;
+
+type TButtonProps = {};
+
+defineProps<TButtonProps>();
+</script>
+
+<style>
+/* HOVER */
+/* FOCUS */
+/* SELECTED */
+/* DISABLED */
+</style>
