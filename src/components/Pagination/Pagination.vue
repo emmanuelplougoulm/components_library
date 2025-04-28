9f6b2f23fe8e657a9b026ef3fdcdeef90c01a2ff
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-28 15:58:17 +0200

    [GFE]: add <Pagination/> boilerplate

---
+++ src/components/Pagination/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+type TPaginationProps = {};
+
+defineProps<TPaginationProps>();
+</script>
+
+<style>
+:root {
+  --color-background: #0a0a0a;
+}
+
+/* BASE STYLES */
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
