905204afc199dcc0a9b3193e4260b599ec93085c
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-04-29 14:22:33 +0200

    [GFE]: add <Pagination/> logic + test display

--- src/components/Pagination/Pagination.vue
+++ src/components/Pagination/Pagination.vue
@@ -1,11 +1,27 @@
 <template>
-  <div></div>
+  <div class="pagination-container">
+    <PageItem v-for="page in pages" :key="page" @click="changePage(page)" :text="page" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-type TPaginationProps = {};
+import { ref, computed } from 'vue';
+import PageItem from './PageItem.vue';
 
-defineProps<TPaginationProps>();
+type TPaginationProps<T> = {
+  items: Array<T>;
+  itemsPerPage: number;
+};
+
+const { items, itemsPerPage } = defineProps<TPaginationProps<{}>>();
+
+const currentPage = ref(1);
+const pageCount = computed(() => Math.ceil(items.length / itemsPerPage));
+const pages = computed(() => Array.from({ length: pageCount.value }, (_, i) => i + 1));
+
+const changePage = (pageNumber) => {
+  currentPage.value = pageNumber;
+};
 </script>
 
 <style>
@@ -18,5 +34,10 @@ defineProps<TPaginationProps>();
 * {
   box-sizing: border-box;
   margin: 0;
+}
+
+.pagination-container {
+  display: flex;
+  gap: 8px;
 }
 </style>
