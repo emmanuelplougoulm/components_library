37538a4a6201f4541dd119ea1f3db7a10adf750d
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-06 17:46:34 +0200

    [GFE]: add <Modal/> for test purposes

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import BaseModal from '@components/BaseModal/BaseModal.vue';
+
+const showModal = ref(false);
+</script>
+
+<template>
+  <button @click="showModal = true">Open</button>
+  <BaseModal :show="showModal" @close="showModal = false">
+    <h2>Title</h2>
+    <p>Content</p>
+    <button @click="showModal = false">Close</button>
+  </BaseModal>
+</template>
