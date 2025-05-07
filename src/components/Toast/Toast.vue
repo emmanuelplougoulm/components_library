00fb51d27a90e63275789a7d64aa3c9f90c6d147
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-07 18:26:44 +0200

    [GFE]: add Toast boilerplate

---
+++ src/components/Toast/Toast.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="toast" id="toast">
+    <div class="toast__content">
+      <div class="toast__content__badge"></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type TToastProps = {};
+
+const uniqueId = `input-${Math.random().toString(36)}`;
+defineProps<TToastProps>();
+</script>
+
+<style>
+/* VARIABLES */
+:root {
+}
+
+/* BASE STYLES */
+
+* {
+}
+</style>
