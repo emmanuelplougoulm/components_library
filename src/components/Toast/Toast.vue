dcd4ed2019056665c31484169033a52907b5f78f
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-07 18:38:37 +0200

    [GFE]: add label + status props + populate Astro page to test it

--- src/components/Toast/Toast.vue
+++ src/components/Toast/Toast.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="toast" id="toast">
     <div class="toast__content">
-      <div class="toast__content__badge"></div>
-      <div></div>
+      <div class="toast__content__badge">{{ status }}</div>
+      <div>{{ label }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-type TToastProps = {};
+type TToastProps = {
+  status: string;
+  label: string;
+};
 
 const uniqueId = `input-${Math.random().toString(36)}`;
-defineProps<TToastProps>();
+const { status, label } = defineProps<TToastProps>();
 </script>
 
 <style>
@@ -22,5 +25,36 @@ defineProps<TToastProps>();
 /* BASE STYLES */
 
 * {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.toast__content {
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  padding: 4px 10px 4px 4px;
+  gap: 12px;
+
+  border-radius: 2000px;
+
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 500;
+}
+
+.toast__content__badge {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 2px 10px;
+
+  height: 24px;
+
+  background: #ffffff;
+  box-shadow:
+    0px 1px 3px rgba(0, 0, 0, 0.1),
+    0px 1px 2px rgba(0, 0, 0, 0.06);
+  border-radius: 9999px;
 }
 </style>
