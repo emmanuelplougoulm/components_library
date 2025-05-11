ef6ee241fbbf6389a95ec16ebd6d3334c662d53a
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-11 12:14:29 +0200

    [GFE]: add vite vue plugin for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,26 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test'
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      optimizeDeps: {
+        include: [...(config.optimizeDeps?.include || []), 'vue', '@storybook/vue3'],
+        exclude: [...(config.optimizeDeps?.exclude || []), 'vue-demi']
+      }
+    };
   }
 };
+
 export default config;
